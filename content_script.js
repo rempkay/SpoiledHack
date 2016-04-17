059ff67cb4dbff4b0099e7246f53082aaed9b0ca
Pancham <pk2506@columbia.edu>
2016-04-16 20:18:54 -0400

    Create content_script.js

---
+++ content_script.js
@@ -0,0 +1,22 @@
+ function hideStuff(keywords){
+
+ 	// for every element
+	// get innerHTML
+	// replace innerHTML with replaced version of html
+
+ 	var body = document.querySelector("<body>");
+ 	var bodyHTML = body.innerHTML;
+
+
+	// iterate over all 'keyword's in array, and do this to them:
+
+	for(var i=0; i<keywords.length;i++){
+		var regularExpression = keywords[i];
+		var index = bodyHTML.search(regularExpression);
+		if(index != -1){
+		  var startIndex = index;
+		  var endIndex = index+regularExpression.length;
+	  	}
+
+  }
+}
